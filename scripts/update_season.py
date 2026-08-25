16d01fb13813f27ec5267cb0fdafc7e8c2961164
MarkColonel80 <markcollison2@gmail.com>
2026-08-25 15:40:04 +0100

    Normalize team names after rich update

--- scripts/update_season.py
+++ scripts/update_season.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+import sys
 from update_season_v5 import main
+from normalize_current_team_names import normalize_current_team_names
 
 if __name__=="__main__":
+    season_dir=sys.argv[1] if len(sys.argv)>1 else None
     main()
+    if season_dir:
+        season=season_dir[:4]+"/"+season_dir[-2:]
+        normalize_current_team_names(season)
